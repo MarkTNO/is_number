51fc62e5cced0fcb552fd921c54ab7d3e55dccb8
Vrijlandt <mark.vrijlandt@tno.nl>
2022-06-25 22:15:03 +0200

    Add GitHub Actions workflow

---
+++ tests/test_is_number.py
@@ -0,0 +1,14 @@
+from is_number import is_number
+
+from datetime import datetime
+
+
+def test_is_number():
+    assert is_number(1)
+
+
+def test_is_not_number():
+    assert not is_number("Hello world")
+    assert not is_number({"Hello": "world"})
+    assert not is_number(datetime.now())
+    assert not is_number(lambda foo: foo)
